17cdf0cc328f89a61428fb6153344d0cd9e738d4
Jyotsana-Sharma <jyotsanasharma212@gmail.com>
2024-04-04 02:25:40 +0530

    added data validation files

--- perceptron/main.py
+++ perceptron/main.py
@@ -1,5 +1,6 @@
 from perceptronBasics.logging import logger
 from perceptronBasics.pipeline.stage_01_data_ingestion_pipleline import DataIngestionPipeline
+from perceptronBasics.pipeline.stage_02_data_validation_pipeline import DataValidationPipeline
 
 
 
@@ -9,6 +10,13 @@ STAGE_NAME = "Data Ingestion Stage"
 logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
 data_ingestion = DataIngestionPipeline()
 data_ingestion.main()
+logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
+
+STAGE_NAME = "Data Validation  Stage"
+
+logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+data_validation = DataValidationPipeline()
+data_validation.main()
 logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<")
 
 
--- perceptron/src/perceptronBasics/config/configuration.py
+++ perceptron/src/perceptronBasics/config/configuration.py
@@ -1,6 +1,6 @@
 from perceptronBasics.constants import *
 from perceptronBasics.utils.common import read_yaml, create_directories
-from perceptronBasics.entity import DataIngestionConfig
+from perceptronBasics.entity import DataIngestionConfig, DataValidationConfig
 
 class ConfigurationManager:
     def __init__(self,config_file_path = CONFIG_FILE_PATH , param_file_path = PARAMS_FILE_PATH):
@@ -18,3 +18,12 @@ class ConfigurationManager:
         data_ingestion_config = DataIngestionConfig(root_dir=config.root_dir, source_URL=config.source_URL, local_data_file=config.local_data_file)
 
         return data_ingestion_config
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+
+        config = self.config.data_validation
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(root_dir = config.root_dir, STATUS_FILE= config.STATUS_FILE, ALL_REQUIRED_FILES= config.ALL_REQUIRED_FILES)
+
+        return data_validation_config
